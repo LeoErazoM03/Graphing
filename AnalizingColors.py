1fe0490c49dab2ec60155be16ef16d23c9bbe569
Erazo <leodan2003@gmail.com>
2024-09-25 21:34:39 -0600

    Captura de colores 2ra parte usando video

--- AnalizingColors.py
+++ AnalizingColors.py
@@ -1,24 +1,24 @@
+import numpy as np
 import cv2 as cv
 
-img = cv.imread('man1.jpeg',1)
+cap = cv.VideoCapture(0)
 
-hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-uba = (15,255,255)
-ubb = (0, 40,40)
-uba2 = (180, 255, 255)
-ubb2 = (170, 40, 40)
+while(True):
+    
+    ret, img = cap.read()
+    if ret:
+        cv.imshow('video', img)
+        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        uba = (15,255,255)
+        ubb = (0, 40,40)
+        mask = cv.inRange(hsv, ubb, uba)
+        res = cv.bitwise_and(img, img, mask=mask)
+        cv.imshow('res', res)
 
-mask1 = cv.inRange(hsv, ubb, uba)
-mask2 = cv.inRange(hsv, ubb2, uba2)
-mask = mask1 + mask2
-
-res = cv.bitwise_and(img, img, mask=mask)
-
-cv.imshow('mask1', mask1)
-cv.imshow('mask2 ', mask2)
-cv.imshow('res', res)
-cv.imshow('hsv', hsv)
-cv.imshow('img', img)
-
-cv.waitKey()
+        k = cv.waitKey(1) & 0xFF
+        if k == 27:
+            break
+    else:
+            break
+cap.release()
 cv.destroyAllWindows()
